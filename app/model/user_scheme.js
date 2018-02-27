f6282dee0d7f6534859f4414afc3b794db73e849
olegmusin <superfilch@yandex.by>
2018-02-27 18:04:25 +0300

    minor fix

--- app/model/user_scheme.js
+++ app/model/user_scheme.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -26,19 +26,15 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.statics.authenticate = function(email, password, callback) {
   User.findOne({ email: email }).exec(function(err, user) {
-    if (err) {
-      return callback(err);
-    } else if (!user) {
+    if (err) return callback(err);
+    else if (!user) {
       var err = new Error("User not found.");
       err.status = 401;
       return callback(err);
     }
     bcrypt.compare(password, user.password, function(err, result) {
-      if (result === true) {
-        return callback(null, user);
-      } else {
-        return callback();
-      }
+      if (result) return callback(null, user);
+      else return callback();
     });
   });
 };
@@ -46,9 +42,7 @@ UserSchema.statics.authenticate = function(email, password, callback) {
 UserSchema.pre("save", function(next) {
   var user = this;
   bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
     user.password = hash;
     next();
   });
